8842d52ed6c00b359f6e62a443c267f1a484489c
martin-codex3 <martinsilungwe12@gmail.com>
2026-01-13 08:52:27 +0200

    refactoring the program.cs file

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -70,7 +70,19 @@ app.MapGet("/products", () => products);
 
 // getting a single product here
 // /products/{id}
-app.MapGet("/products/{id}", (int id) => products.Find(product => product.ProductId == id)).WithName(ApiRouteName);
+app.MapGet("/products/{id}", (int id) =>
+{
+    var product = products.Find(product => product.ProductId == id);
+
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        return Results.Ok(product);
+    }
+}).WithName(ApiRouteName);
 
 // adding a new product here
 app.MapPost("/products", (CreateProductDto newProduct) =>
@@ -101,6 +113,11 @@ app.MapPut("/products/{id}", (int id, UpdateProductDto updateProductDto) =>
    // we will have to get the product that we want to update first
    var index = products.FindIndex(product => product.ProductId == id);
 
+   if (index == -1)
+    {
+        return Results.NotFound();
+    }
+
    products[index] = new ProductDto(
     id,
     updateProductDto.ProductTitle,
@@ -111,8 +128,19 @@ app.MapPut("/products/{id}", (int id, UpdateProductDto updateProductDto) =>
     updateProductDto.CreatedAt,
     updateProductDto.UpdatedAt
    );
-   
 
+
+   // we will return the updated product here
+   return Results.NoContent();
+});
+
+
+app.MapDelete("/products/{id}", (int id) =>
+{
+    // we will attempt to delete the product here
+    products.RemoveAll(product => product.ProductId == id);
+
+    return Results.NoContent(); 
 });
 
 
--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c6a6c6f45384d257f4d86aa8e812975fa792e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc0db9b3075a29085bb2160052e5cc6aab833b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
